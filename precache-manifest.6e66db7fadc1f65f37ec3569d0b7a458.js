4de4195e0cada8f28587080461929b8dd934be94
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-15 20:25:26 +0000

    Deploy mikhail-shpakov/fetch-github-profiles-with-vue-rx to github.com/mikhail-shpakov/fetch-github-profiles-with-vue-rx.git:gh-pages

---
+++ precache-manifest.6e66db7fadc1f65f37ec3569d0b7a458.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e5625f71f3a03deded",
+    "url": "/fetch-github-profiles-with-vue-rx/css/app.ccf29976.css"
+  },
+  {
+    "revision": "4dade9967ead64ef2ae9",
+    "url": "/fetch-github-profiles-with-vue-rx/css/chunk-vendors.12ef1a11.css"
+  },
+  {
+    "revision": "6b0ef144c112861309b4632f52a24774",
+    "url": "/fetch-github-profiles-with-vue-rx/img/empty.6b0ef144.svg"
+  },
+  {
+    "revision": "2c1ecc8d66aaa728733cd5be4efbd7e4",
+    "url": "/fetch-github-profiles-with-vue-rx/img/logo.2c1ecc8d.svg"
+  },
+  {
+    "revision": "5210e710680b328eb7506076a8188362",
+    "url": "/fetch-github-profiles-with-vue-rx/img/profiles.5210e710.svg"
+  },
+  {
+    "revision": "6e7dad634ad894d9af76ae8379b446b5",
+    "url": "/fetch-github-profiles-with-vue-rx/index.html"
+  },
+  {
+    "revision": "58e5625f71f3a03deded",
+    "url": "/fetch-github-profiles-with-vue-rx/js/app.83eecca5.js"
+  },
+  {
+    "revision": "4dade9967ead64ef2ae9",
+    "url": "/fetch-github-profiles-with-vue-rx/js/chunk-vendors.dd6edf97.js"
+  },
+  {
+    "revision": "ff9fe3059617dae29c5b47d654f19d94",
+    "url": "/fetch-github-profiles-with-vue-rx/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fetch-github-profiles-with-vue-rx/robots.txt"
+  }
+]);
